fe488430ffc64b00e9bf8219edb0f21a57a0af85
JB-Tellez <jb.tellez@gmail.com>
2021-09-23 09:55:10 -0700

    adds GET home route

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 'use strict';
 
+// imports
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 
-
+// globals
+const PORT = process.env.PORT;
 const app = express();
 
+
+// middleware
 app.use(cors());
 
-app.listen(3000, () => console.log('Listening'));
+
+
+// routes
+app.get('/', (request, response) => {
+  console.log('You called? I am a GET at home route');
+  response.status(200).send('Hi hello haha');
+
+});
+
+
+
+// listen
+app.listen(PORT, () => console.log('Listening on PORT ' + PORT));
 
